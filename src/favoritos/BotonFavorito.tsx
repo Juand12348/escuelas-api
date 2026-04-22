9f17d65c7ef401b646bd033f1d745e7b88d5847f
Juand12348 <156850435+Juand12348@users.noreply.github.com>
2026-04-22 23:09:58 +0000

    Agregado estilos css boton favoritos

--- src/favoritos/BotonFavorito.tsx
+++ src/favoritos/BotonFavorito.tsx
@@ -1,33 +1,21 @@
-// BotonFavorito.tsx
-
 import { useEffect, useState } from "react";
+import "./style.css";
 
-export default function BotonFavorito({
-  producto,
-}: any) {
-
-  const [esFavorito, setEsFavorito] =
-    useState(false);
+export default function BotonFavorito({ producto }: any) {
+  const [esFavorito, setEsFavorito] = useState(false);
 
   useEffect(() => {
-
-    const favoritos = JSON.parse(
-      localStorage.getItem("favoritos") || "[]"
-    );
+    const favoritos = JSON.parse(localStorage.getItem("favoritos") || "[]");
 
     const existe = favoritos.find(
       (item: any) => item.id === producto.id
     );
 
     setEsFavorito(!!existe);
-
   }, [producto.id]);
 
   const manejarFavorito = () => {
-
-    const favoritos = JSON.parse(
-      localStorage.getItem("favoritos") || "[]"
-    );
+    const favoritos = JSON.parse(localStorage.getItem("favoritos") || "[]");
 
     const existe = favoritos.find(
       (item: any) => item.id === producto.id
@@ -36,34 +24,24 @@ export default function BotonFavorito({
     let nuevosFavoritos = [];
 
     if (existe) {
-
       nuevosFavoritos = favoritos.filter(
         (item: any) => item.id !== producto.id
       );
-
       setEsFavorito(false);
-
     } else {
-
-      nuevosFavoritos = [
-        ...favoritos,
-        producto,
-      ];
-
+      nuevosFavoritos = [...favoritos, producto];
       setEsFavorito(true);
     }
 
-    localStorage.setItem(
-      "favoritos",
-      JSON.stringify(nuevosFavoritos)
-    );
+    localStorage.setItem("favoritos", JSON.stringify(nuevosFavoritos));
   };
 
   return (
-    <button onClick={manejarFavorito}>
-
+    <button
+      onClick={manejarFavorito}
+      className={`btn-favorito ${esFavorito ? "activo" : ""}`}
+    >
       {esFavorito ? "❤️" : "🤍"}
-
     </button>
   );
 }
